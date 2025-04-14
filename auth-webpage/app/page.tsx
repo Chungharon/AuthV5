a9f2f528d9bead017bfdd43ecbb9c56fa249c406
vinter <134591065+Anittavinter@users.noreply.github.com>
2025-04-14 16:59:10 +0300

    Home Page

--- auth-webpage/app/page.tsx
+++ auth-webpage/app/page.tsx
@@ -1,6 +1,34 @@
 import { Button } from "@/components/ui/button";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import LoginButton from "@/components/auth/login-button";
+import { loadGetInitialProps } from "next/dist/shared/lib/utils";
+
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
 export default function Home() {
   return (
-    <p>Hellow</p>
+    <main className="flex h-full flex-col items-center justify-center bg-stone-500">
+      <div className="space-y-6 text-center">
+        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md",
+          font.className,
+        )}>
+          Auth
+        </h1>
+        <p className="text-white text-lg">
+          Authentication Service
+        </p>
+        <div>
+          <LoginButton>
+           <Button variant={"secondary"} size={"lg"}>
+            Sign IN
+           </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
